df38fe0c5bff047a224dc00ffc8b58c2200a68f1
Semih Turkoglu <semih.trkgl99@gmail.com>
2026-04-13 13:19:24 +0200

    feat : partie b) exercice 2.D

--- TP4/ex4.c
+++ TP4/ex4.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include "utils.h"
 
 #define MAX 20
@@ -9,5 +10,6 @@ int main (int argc, char** argv) {
     printf("saisissez le nom du fichier :\n");
     char bufRd[MAX];
     int nbChar = sread(0, bufRd, MAX);
-    
+    bufRd[nbChar - 1] = '\0';
+    int fd = sopen(bufRd, O_RDWR |  O_CREAT | O_TRUNC, 0744);
 }
