1add05a39255a3dfa48a44976566e7f52df84b47
Semih Turkoglu <semih.trkgl99@gmail.com>
2026-04-13 12:12:34 +0200

    refactor: 3rd part of TP4

--- TP4/ex3d.c
+++ TP4/ex3d.c
@@ -3,16 +3,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <string.h>
 #include "utils.h"
 
+
+void childFunction() {
+    const char* str1 = "4 5 6\n";
+    nwrite(1, str1, strlen(str1));
+    exit(13);
+}
+
 int main () {
-    int childId = sfork();
-    if(childId == 0) {
-        printf("4 5 6\n");
-        exit(13);
+    int childId = fork_and_run0(childFunction);
+    int childStatus;
+    swaitpid(childId, &childStatus, 0);
+
+    const char* str2 = "1 2 3\n";
+    nwrite(1, str2, strlen(str2));
+
+    if (WIFEXITED(childStatus)) {
+        printf("child process exited with code : %d\n", WEXITSTATUS(childStatus));
     } else {
-        int exitStatus;
-        wait(&exitStatus);
-        printf("1 2 3\n%d\n", WEXITSTATUS(exitStatus));
+        printf("error : child process did not exited the program correctly\n");
     }
 }
