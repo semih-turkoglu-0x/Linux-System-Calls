cb5993f3de1cdf0615767db1003d5b2ba89efec7
Semih Turkoglu <semih.trkgl99@gmail.com>
2026-04-14 12:10:09 +0200

    feat: exercie 2.D part c)

--- TP4/ex4.c
+++ TP4/ex4.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 #include "utils.h"
 
 #define MAX 20
@@ -11,5 +12,23 @@ int main (int argc, char** argv) {
     char bufRd[MAX];
     int nbChar = sread(0, bufRd, MAX);
     bufRd[nbChar - 1] = '\0';
-    int fd = sopen(bufRd, O_RDWR |  O_CREAT | O_TRUNC, 0744);
+    sopen(bufRd, O_RDWR |  O_CREAT | O_TRUNC, 0744);
+
+    int childPid = fork();
+    int childStatus;
+
+    if (childPid == -1) {
+        perror("fork error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (!childPid) {
+        char* args[] = {"ls", "-l", NULL};
+        execv("/bin/ls", args);
+        perror("error execv\n");
+        exit(EXIT_FAILURE);
+        } else {
+        waitpid(childPid, &childStatus, 0);
+        exit(EXIT_SUCCESS); 
+    }
 }
