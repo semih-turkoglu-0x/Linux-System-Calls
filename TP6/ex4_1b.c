94ff5f99acde76e080022e861efae63dcfd4ea8f
Semih Turkoglu <semih.trkgl99@gmail.com>
2026-04-16 12:38:16 +0200

    feat : exercice 4.1.b)

---
+++ TP6/ex4_1b.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "utils.h"
+
+void sigusr1_handler (int sig) {
+    const char* msg = "signal SIGUSR1 recu !\n";
+    const size_t sz = strlen (msg);
+    swrite(1, msg, sz);
+    _exit(13);
+}
+
+int main () {
+    struct sigaction action = {0};
+    action.sa_handler = sigusr1_handler;
+    
+    int ret = sigaction(SIGUSR1, &action, NULL);
+    checkNeg(ret, "error sigaction");
+
+    pid_t childPid = sfork();
+    if (childPid) {
+        ret = kill(childPid, SIGUSR1);
+        checkNeg(ret, "kill error");
+        swait(NULL);
+        exit(EXIT_SUCCESS);
+    } else {
+        exit(0);
+    }
+}
